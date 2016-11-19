be63cf8fe36b690e7aec43bb1ec5d6fb8a70dc9f
CLEMENG <CLEMENG@LT053099>
2016-11-19 10:49:50 +0000

    Add project files.

---
+++ BulkLoad/stg.BulkFile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [stg].[BulkFile]
+(
+	[pkBulkFile] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Column01] VARCHAR(100) NULL, 
+    [Column02] VARCHAR(100) NULL, 
+    [Column03] VARCHAR(100) NULL, 
+    [Column04] VARCHAR(100) NULL, 
+    [Column05] VARCHAR(100) NULL, 
+    [Column06] VARCHAR(100) NULL, 
+    [Column07] VARCHAR(100) NULL, 
+    [Column08] VARCHAR(100) NULL, 
+    [Column09] VARCHAR(100) NULL
+)
